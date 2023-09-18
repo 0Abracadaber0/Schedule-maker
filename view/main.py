fa436f94dbac141531d01f475a9281ea0cf8b759
Yauheni <yauhenbelarus@gmail.com>
2023-09-18 09:00:40 +0300

    add main in view

---
+++ view/main.py
@@ -0,0 +1,21 @@
+import sys
+import design
+
+from PyQt5 import QtWidgets
+
+
+class App(QtWidgets.QMainWindow, design.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = App()
+    window.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
